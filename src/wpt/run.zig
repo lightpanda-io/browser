e4b4719208b6cacb36f0771443ec9957bc5ecb9b
Pierre Tachoire <pierre@lightpanda.io>
2024-02-12 12:12:42 +0100

    wpt: defer console log display

--- src/wpt/run.zig
+++ src/wpt/run.zig
@@ -40,12 +40,19 @@ pub fn run(arena: *std.heap.ArenaAllocator, comptime dir: []const u8, f: []const
     try js_env.start(alloc);
     defer js_env.stop();
 
-    // add document object
-    try js_env.addObject(html_doc, "document");
+    // display console logs
+    defer {
+        var res = evalJS(js_env, alloc, "console.join('\\n');", "console") catch unreachable;
+        defer res.deinit(alloc);
+        if (res.result.len > 0) {
+            std.debug.print("-- CONSOLE LOG\n{s}\n--\n", .{res.result});
+        }
+    }
 
-    // alias global as self and window
+    // setup global env vars.
     try js_env.attachObject(try js_env.getGlobal(), "self", null);
     try js_env.attachObject(try js_env.getGlobal(), "window", null);
+    try js_env.addObject(html_doc, "document");
 
     // thanks to the arena, we don't need to deinit res.
     var res: jsruntime.JSResult = undefined;
@@ -112,12 +119,6 @@ pub fn run(arena: *std.heap.ArenaAllocator, comptime dir: []const u8, f: []const
     res = try evalJS(js_env, alloc, "window.dispatchEvent({target: 'load'});", "ready");
     if (!res.success) {
         return res;
-    }
-
-    // display console logs
-    res = try evalJS(js_env, alloc, "console.join(', ');", "console");
-    if (res.result.len > 0) {
-        std.debug.print("-- CONSOLE LOG\n{s}\n--\n", .{res.result});
     }
 
     // Check the final test status.
