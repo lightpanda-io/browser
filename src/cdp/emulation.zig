35e2078f8a6560a6d4e7515d0609a2855e1c9238
Francis Bouvier <francis@lightpanda.io>
2024-04-18 11:32:11 +0200

    Return sessionId in Emulation.setFocusEmulationEnabled
    Signed-off-by: Francis Bouvier <francis@lightpanda.io>

--- src/cdp/emulation.zig
+++ src/cdp/emulation.zig
@@ -2,9 +2,10 @@ const std = @import("std");
 
 const server = @import("../server.zig");
 const Ctx = server.Cmd;
-const result = @import("cdp.zig").result;
-const getParams = @import("cdp.zig").getParams;
-const stringify = @import("cdp.zig").stringify;
+const cdp = @import("cdp.zig");
+const result = cdp.result;
+const getParams = cdp.getParams;
+const stringify = cdp.stringify;
 
 const EmulationMethods = enum {
     setEmulatedMedia,
@@ -38,8 +39,18 @@ fn setEmulatedMedia(
 fn setFocusEmulationEnabled(
     alloc: std.mem.Allocator,
     id: u64,
-    _: *std.json.Scanner,
+    scanner: *std.json.Scanner,
     _: *Ctx,
 ) ![]const u8 {
-    return result(alloc, id, null, null, null);
+
+    // input
+    const Params = struct {
+        enabled: bool,
+    };
+    _ = try getParams(alloc, Params, scanner);
+    const sessionID = try cdp.getSessionID(scanner);
+
+    // output
+    // TODO: dummy
+    return result(alloc, id, null, null, sessionID);
 }
