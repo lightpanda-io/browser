62805cdf1daff042ce06aad1712816d19fcc71b3
Karl Seguin <k@openmymind.io>
2025-02-08 20:10:26 +0800

    add license to file

--- src/unit_tests.zig
+++ src/unit_tests.zig
@@ -1,13 +1,20 @@
-// Changed Jan 29, 2025 to accomodate latest Zig changes
-// See history if you're using an older version of Zig.
-
-// in your build.zig, you can specify a custom test runner:
-// const tests = b.addTest(.{
-//   .target = target,
-//   .optimize = optimize,
-//   .test_runner = .{ .path = b.path("test_runner.zig"), .mode = .simple }, // add this line
-//   .root_source_file = b.path("src/main.zig"),
-// });
+// Copyright (C) 2023-2024  Lightpanda (Selecy SAS)
+//
+// Francis Bouvier <francis@lightpanda.io>
+// Pierre Tachoire <pierre@lightpanda.io>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 const std = @import("std");
 const builtin = @import("builtin");
