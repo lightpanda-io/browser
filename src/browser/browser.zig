ab974751f0fb1114e122a9435ec1991381a37f6b
Karl Seguin <k@openmymind.io>
2025-03-24 15:58:39 +0800

    Add cookie support to browser (not XHR yet) requests

--- src/browser/browser.zig
+++ src/browser/browser.zig
@@ -43,7 +43,7 @@ const Location = @import("../html/location.zig").Location;
 
 const storage = @import("../storage/storage.zig");
 
-const HttpClient = @import("../http/client.zig").Client;
+const http = @import("../http/client.zig");
 const UserContext = @import("../user_context.zig").UserContext;
 
 const polyfill = @import("../polyfill/polyfill.zig");
@@ -60,7 +60,7 @@ pub const Browser = struct {
     app: *App,
     session: ?*Session,
     allocator: Allocator,
-    http_client: *HttpClient,
+    http_client: *http.Client,
     session_pool: SessionPool,
     page_arena: std.heap.ArenaAllocator,
 
@@ -123,10 +123,12 @@ pub const Session = struct {
 
     window: Window,
 
-    // TODO move the shed to the browser?
+    // TODO move the shed/jar to the browser?
     storage_shed: storage.Shed,
+    cookie_jar: storage.CookieJar,
+
     page: ?Page = null,
-    http_client: *HttpClient,
+    http_client: *http.Client,
 
     jstypes: [Types.len]usize = undefined,
 
@@ -141,6 +143,7 @@ pub const Session = struct {
             .http_client = browser.http_client,
             .storage_shed = storage.Shed.init(allocator),
             .arena = std.heap.ArenaAllocator.init(allocator),
+            .cookie_jar = storage.CookieJar.init(allocator),
             .window = Window.create(null, .{ .agent = user_agent }),
         };
 
@@ -176,6 +179,7 @@ pub const Session = struct {
         }
         self.env.deinit();
         self.arena.deinit();
+        self.cookie_jar.deinit();
         self.storage_shed.deinit();
     }
 
@@ -364,14 +368,16 @@ pub const Page = struct {
         } });
 
         // load the data
-        var request = try self.session.http_client.request(.GET, self.uri);
+        var request = try self.newHTTPRequest(.GET, self.uri, .{ .navigation = true });
         defer request.deinit();
-        var response = try request.sendSync(.{});
 
+        var response = try request.sendSync(.{});
         const header = response.header;
+        try self.processHTTPResponse(self.uri, &header);
+
         log.info("GET {any} {d}", .{ self.uri, header.status });
 
-        const ct = response.header.get("content-type") orelse {
+        const ct = header.get("content-type") orelse {
             // no content type in HTTP headers.
             // TODO try to sniff mime type from the body.
             log.info("no content-type HTTP header", .{});
@@ -607,13 +613,19 @@ pub const Page = struct {
         }
         const u = try std.Uri.resolve_inplace(self.uri, res_src, &b);
 
-        var request = try self.session.http_client.request(.GET, u);
+        var request = try self.newHTTPRequest(.GET, u, .{
+            .origin = self.uri,
+            .navigation = false,
+        });
         defer request.deinit();
+
         var response = try request.sendSync(.{});
+        var header = response.header;
+        try self.processHTTPResponse(u, &header);
 
-        log.info("fetch {any}: {d}", .{ u, response.header.status });
+        log.info("fetch {any}: {d}", .{ u, header.status });
 
-        if (response.header.status != 200) {
+        if (header.status != 200) {
             return FetchError.BadStatusCode;
         }
 
@@ -636,6 +648,46 @@ pub const Page = struct {
         const arena = self.arena;
         const body = try self.fetchData(arena, s.src, null);
         try s.eval(arena, &self.session.env, body);
+    }
+
+    const RequestOpts = struct {
+        origin: ?std.Uri = null,
+        navigation: bool = true,
+    };
+    fn newHTTPRequest(self: *const Page, method: http.Request.Method, uri: std.Uri, opts: RequestOpts) !http.Request {
+        const session = self.session;
+        var request = try session.http_client.request(method, uri);
+        errdefer request.deinit();
+
+        var cookies = try session.cookie_jar.forRequest(
+            self.arena,
+            std.time.timestamp(),
+            opts.origin,
+            uri,
+            opts.navigation,
+        );
+        defer cookies.deinit(self.arena);
+
+        if (cookies.len() > 0) {
+            var arr: std.ArrayListUnmanaged(u8) = .{};
+            try cookies.write(arr.writer(self.arena));
+            try request.addHeader("Cookie", arr.items, .{});
+        }
+
+        return request;
+    }
+
+    fn processHTTPResponse(self: *const Page, uri: std.Uri, header: *const http.ResponseHeader) !void {
+        const session = self.session;
+        const now = std.time.timestamp();
+        var it = header.iterate("set-cookie");
+        while (it.next()) |set_cookie| {
+            const c = storage.Cookie.parse(self.arena, uri, set_cookie) catch |err| {
+                log.warn("Couldn't parse cookie '{s}': {}\n", .{ set_cookie, err });
+                continue;
+            };
+            try session.cookie_jar.add(c, now);
+        }
     }
 
     const Script = struct {
