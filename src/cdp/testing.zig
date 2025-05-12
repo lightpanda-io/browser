9ab8a2cbd272c538269313a46e9618cc646d09bb
sjorsdonkers <72333389+sjorsdonkers@users.noreply.github.com>
2025-05-12 11:34:57 +0200

    remove manual test parser.init

--- src/cdp/testing.zig
+++ src/cdp/testing.zig
@@ -121,7 +121,6 @@ const TestContext = struct {
         if (opts.html) |html| {
             if (bc.session_id == null) bc.session_id = "SID-X";
             parser.deinit();
-            try parser.init();
             const page = try bc.session.createPage();
             page.doc = (try Document.init(html)).doc;
         }
