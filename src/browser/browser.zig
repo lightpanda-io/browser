42d0532580f4804cdab7e0ea2f4a278da744603c
sjorsdonkers <72333389+sjorsdonkers@users.noreply.github.com>
2025-05-13 14:38:27 +0200

    cleanup

--- src/browser/browser.zig
+++ src/browser/browser.zig
@@ -251,7 +251,7 @@ pub const Page = struct {
     fn init(self: *Page, arena: Allocator, session: *Session) !void {
         const browser = session.browser;
         self.* = .{
-            .window = try Window.create(null, null), // TODO why do we not call Window.create()?
+            .window = try Window.create(null, null),
             .arena = arena,
             .doc = null,
             .raw_data = null,
