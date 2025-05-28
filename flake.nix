f49710f361b886f665b353264559fb1eaeba95db
Muki Kiboigo <muki@muki.gg>
2025-05-28 13:02:25 -0700

    update flake.nix for Zig 0.14.1

--- flake.nix
+++ flake.nix
@@ -2,37 +2,25 @@
   description = "headless browser designed for AI and automation";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
-
-    iguana.url = "github:mookums/iguana";
-    iguana.inputs.nixpkgs.follows = "nixpkgs";
-
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
     {
       nixpkgs,
-      iguana,
       flake-utils,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        zigVersion = "0_14_0";
-        iguanaLib = iguana.lib.${system};
-
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [
-            (iguanaLib.mkZigOverlay zigVersion)
-            (iguanaLib.mkZlsOverlay zigVersion)
-          ];
         };
 
         # This build pipeline is very unhappy without an FHS-compliant env.
-        fhs = pkgs.buildFHSUserEnv {
+        fhs = pkgs.buildFHSEnv {
           name = "fhs-shell";
           targetPkgs =
             pkgs: with pkgs; [
