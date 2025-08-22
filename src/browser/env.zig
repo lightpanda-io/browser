02c476aee184f58d20afddc3df7c53926cac7eab
Muki Kiboigo <muki@muki.gg>
2025-08-22 06:46:09 -0700

    initial fetch in zig

--- src/browser/env.zig
+++ src/browser/env.zig
@@ -35,8 +35,9 @@ const WebApis = struct {
         @import("xhr/xhr.zig").Interfaces,
         @import("xhr/form_data.zig").Interfaces,
         @import("xmlserializer/xmlserializer.zig").Interfaces,
-        @import("fetch/Request.zig"),
         @import("fetch/Headers.zig"),
+        @import("fetch/Request.zig"),
+        @import("fetch/Response.zig"),
     });
 };
 
