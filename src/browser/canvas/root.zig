c459325a5f91c51445754c3ee995d584207d8978
Halil Durak <halildrk@gmail.com>
2025-11-13 14:55:12 +0300

    update `CanvasRenderingContext2D` test
    Adds the missing RGBA and long digit hex format tests.

--- src/browser/canvas/root.zig
+++ src/browser/canvas/root.zig
@@ -1,11 +1,12 @@
 //! Canvas API.
 //! https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
 
+const CanvasRenderingContext2D = @import("CanvasRenderingContext2D.zig");
 const WebGLRenderingContext = @import("WebGLRenderingContext.zig");
-const ExtensionType = WebGLRenderingContext.Extension.Type;
+const Extension = WebGLRenderingContext.Extension;
 
 pub const Interfaces = .{
-    @import("CanvasRenderingContext2D.zig"),
+    CanvasRenderingContext2D,
     WebGLRenderingContext,
-    ExtensionType.WEBGL_debug_renderer_info,
+    Extension.Type.WEBGL_debug_renderer_info,
 };
