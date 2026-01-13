63f1c859645c18e5032c7b8855f9381debaf940e
Karl Seguin <k@openmymind.io>
2026-01-13 18:09:58 +0800

    Remove unnecessary microtask run.
    This crashes linux in releasesafe without an embedded snapshot. Not sure why,
    but it shouldn't be necessary. This was added back when we were executing
    microtasks on a schedule, rather than manually at explicit points.

--- src/browser/js/ExecutionWorld.zig
+++ src/browser/js/ExecutionWorld.zig
@@ -143,12 +143,6 @@ pub fn createContext(self: *ExecutionWorld, page: *Page, enter: bool) !*Context 
 }
 
 pub fn removeContext(self: *ExecutionWorld) void {
-    // Force running the micro task to drain the queue before reseting the
-    // context arena.
-    // Tasks in the queue are relying to the arena memory could be present in
-    // the queue. Running them later could lead to invalid memory accesses.
-    self.env.runMicrotasks();
-
     var context = &(self.context orelse return);
     context.deinit();
     self.context = null;
