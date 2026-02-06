aea2b3c8e5f8fcdd6513c49c8ab238910692f58b
Karl Seguin <k@openmymind.io>
2026-02-07 07:26:02 +0800

    remove empty_with_no_proto from console, since it isn't empty

--- src/browser/webapi/Console.zig
+++ src/browser/webapi/Console.zig
@@ -167,7 +167,6 @@ pub const JsApi = struct {
 
         pub const prototype_chain = bridge.prototypeChain();
         pub var class_id: bridge.ClassId = undefined;
-        pub const empty_with_no_proto = true;
     };
 
     pub const trace = bridge.function(Console.trace, .{});
