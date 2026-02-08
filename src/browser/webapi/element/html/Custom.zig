cccb45fe1380a8fe9cf1111335cbd7a87869f2d0
Karl Seguin <k@openmymind.io>
2026-02-08 09:55:18 +0800

    Remove the "TODO-CUSTOM-NAME" name on custom elements
    Giving a WebAPI a JsApi.Meta.name registers that name on the global object, e.g
    window['TODO-CUSTOM-NAME'] becomes a type.

--- src/browser/webapi/element/html/Custom.zig
+++ src/browser/webapi/element/html/Custom.zig
@@ -241,7 +241,6 @@ pub const JsApi = struct {
     pub const bridge = js.Bridge(Custom);
 
     pub const Meta = struct {
-        pub const name = "TODO-CUSTOM-NAME";
         pub const prototype_chain = bridge.prototypeChain();
         pub var class_id: bridge.ClassId = undefined;
     };
