11ed95290b2abe3a70bfa2d2e670142f8a22927c
Karl Seguin <k@openmymind.io>
2026-03-23 17:09:37 +0800

    Improve async tests
    testing.async(...) is pretty lame. It works for simple cases, where the
    microtask is very quickly resolved, but otherwise can't block the test from
    exiting.
    
    This adds an overload to testing.async and leverages the new Runner
    https://github.com/lightpanda-io/browser/pull/1958 to "tick" until completion
    (or timeout).
    
    The overloaded version of testing.async() (called without a callback) will
    increment a counter which is only decremented with the promise is resolved. The
    test runner will now `tick` until the counter == 0.

--- src/browser/Page.zig
+++ src/browser/Page.zig
@@ -3587,12 +3587,7 @@ test "WebApi: Page" {
 }
 
 test "WebApi: Frames" {
-    // TOO FLAKY, disabled for now
-
-    // const filter: testing.LogFilter = .init(&.{.js});
-    // defer filter.deinit();
-
-    // try testing.htmlRunner("frames", .{});
+    try testing.htmlRunner("frames", .{});
 }
 
 test "WebApi: Integration" {
