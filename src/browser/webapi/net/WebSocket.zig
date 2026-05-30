u2i/lightpanda-browser

Send cookies on WebSocket upgrade requests
The WebSocket upgrade handshake is an HTTP/1.1 request (RFC 6455 §4.1)
and follows ordinary cookie semantics — RFC 6265 §5.4 attaches matching
cookies to "any HTTP request" by domain/path. Without this, cookie-
authenticated WebSocket endpoints (anything session-gated, e.g. Phoenix
LiveView) reject the upgrade because their auth cookie never arrives.

Read matching cookies from the session jar with the same opts shape
HTTPDocument uses (`is_http: true, is_navigation: false`), and add a
`Cookie:` request header on the upgrade if any apply.

The TestWSServer captures the upgrade's Cookie header and exposes it
to fixtures via a new `get-cookie` command. A `cookies_on_upgrade`
fixture in websocket.html sets `document.cookie` then asserts the
server received it on the upgrade.

src/browser/webapi/net/WebSocket.zig
@@ -131,9 +131,32 @@ pub fn init(url: []const u8, protocols: [][]const u8, frame: *Frame) !*WebSocket
 
     var headers = try http_client.newHeaders();
     errdefer headers.deinit();
+    var has_extra_headers = false;
+
     if (protocols.len > 0) {
         const header = try std.fmt.allocPrintSentinel(arena, "Sec-WebSocket-Protocol: {s}", .{try std.mem.join(arena, ", ", protocols)}, 0);
         try headers.add(header);
+        has_extra_headers = true;
+    }
+
+    // Attach matching cookies from the session jar. Real browsers send
+    // cookies on the WebSocket upgrade request just like any other
+    // same-origin HTTP request — without this, server-side session
+    // checks (Phoenix LiveView, anything cookie-authenticated) reject
+    // the upgrade.
+    var cookie_buf: std.Io.Writer.Allocating = .init(arena);
+    try frame._session.cookie_jar.forRequest(resolved_url, &cookie_buf.writer, .{
+        .is_http = true,
+        .is_navigation = false,
+        .origin_url = frame.url,
+    });
+    if (cookie_buf.written().len > 0) {
+        const cookie_header = try std.fmt.allocPrintSentinel(arena, "Cookie: {s}", .{cookie_buf.written()}, 0);
+        try headers.add(cookie_header);
+        has_extra_headers = true;
+    }
+
+    if (has_extra_headers) {
         try conn.setHeaders(&headers);
     }
 

lightpanda-io/browser

src/browser/webapi/net/WebSocket.zig
@@ -133,9 +133,32 @@ pub fn init(url: []const u8, protocols: [][]const u8, exec: *const Execution) !*
 
     var headers = try http_client.newHeaders();
     errdefer headers.deinit();
+    var has_extra_headers = false;
+
     if (protocols.len > 0) {
         const header = try std.fmt.allocPrintSentinel(arena, "Sec-WebSocket-Protocol: {s}", .{try std.mem.join(arena, ", ", protocols)}, 0);
         try headers.add(header);
+        has_extra_headers = true;
+    }
+
+    // Attach matching cookies from the session jar. Real browsers send
+    // cookies on the WebSocket upgrade request just like any other
+    // same-origin HTTP request — without this, server-side session
+    // checks (Phoenix LiveView, anything cookie-authenticated) reject
+    // the upgrade.
+    var cookie_buf: std.Io.Writer.Allocating = .init(arena);
+    try exec.session.cookie_jar.forRequest(resolved_url, &cookie_buf.writer, .{
+        .is_http = true,
+        .is_navigation = false,
+        .origin_url = exec.url.*,
+    });
+    if (cookie_buf.written().len > 0) {
+        const cookie_header = try std.fmt.allocPrintSentinel(arena, "Cookie: {s}", .{cookie_buf.written()}, 0);
+        try headers.add(cookie_header);
+        has_extra_headers = true;
+    }
+
+    if (has_extra_headers) {
         try conn.setHeaders(&headers);
     }
 