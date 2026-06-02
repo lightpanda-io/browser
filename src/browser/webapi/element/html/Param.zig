8116214a420170094dbd67b3276ebb8683a0ebaa
Rohit <71192000+rohitsux@users.noreply.github.com>
2026-06-03 00:49:03 +0530

    feat(webapi): reflect name and value on HTMLParamElement
    Add the reflected DOMString attributes name and value to the
    existing HTMLParamElement, per HTML 15.3
    (https://html.spec.whatwg.org/multipage/obsolete.html#htmlparamelement).
    Mirrors the reflection idiom used by HTMLTimeElement / HTMLDataElement.
    
    Advances the html/dom/reflection-obsolete.html WPT row.

--- src/browser/webapi/element/html/Param.zig
+++ src/browser/webapi/element/html/Param.zig
@@ -1,4 +1,5 @@
 const js = @import("../../../js/js.zig");
+const Frame = @import("../../../Frame.zig");
 const Node = @import("../../Node.zig");
 const Element = @import("../../Element.zig");
 const HtmlElement = @import("../Html.zig");
@@ -14,6 +15,22 @@ pub fn asNode(self: *Param) *Node {
     return self.asElement().asNode();
 }
 
+pub fn getName(self: *Param) []const u8 {
+    return self.asElement().getAttributeSafe(comptime .wrap("name")) orelse "";
+}
+
+pub fn setName(self: *Param, value: []const u8, frame: *Frame) !void {
+    try self.asElement().setAttributeSafe(comptime .wrap("name"), .wrap(value), frame);
+}
+
+pub fn getValue(self: *Param) []const u8 {
+    return self.asElement().getAttributeSafe(comptime .wrap("value")) orelse "";
+}
+
+pub fn setValue(self: *Param, value: []const u8, frame: *Frame) !void {
+    try self.asElement().setAttributeSafe(comptime .wrap("value"), .wrap(value), frame);
+}
+
 pub const JsApi = struct {
     pub const bridge = js.Bridge(Param);
 
@@ -22,4 +39,12 @@ pub const JsApi = struct {
         pub const prototype_chain = bridge.prototypeChain();
         pub var class_id: bridge.ClassId = undefined;
     };
+
+    pub const name = bridge.accessor(Param.getName, Param.setName, .{ .ce_reactions = true });
+    pub const value = bridge.accessor(Param.getValue, Param.setValue, .{ .ce_reactions = true });
 };
+
+const testing = @import("../../../../testing.zig");
+test "WebApi: HTML.Param" {
+    try testing.htmlRunner("element/html/param.html", .{});
+}
