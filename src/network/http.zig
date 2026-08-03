b1b9a645731917251efbf5f122670759da69072b
Halil Durak <halildrk@gmail.com>
2026-08-03 12:45:25 +0300

    CLI option for adblock list passing + integrate adblocker to http client

--- src/network/http.zig
+++ src/network/http.zig
@@ -828,6 +828,7 @@ pub const ErrorReason = enum {
     too_large,
     aborted,
     robots_blocked,
+    adblock_blocked,
     other,
 };
 
@@ -855,6 +856,7 @@ pub fn errorReason(err: anyerror) ErrorReason {
         error.SyncWaitInterrupted,
         => .aborted,
         error.RobotsBlocked => .robots_blocked,
+        error.AdblockBlocked => .adblock_blocked,
         else => .other,
     };
 }
