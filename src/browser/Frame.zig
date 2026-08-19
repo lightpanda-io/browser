d508729af8e9538253172650f6c851ee00c8986c
Halil Durak <halildrk@gmail.com>
2026-08-19 17:13:12 +0300

    `Frame`: remove `unreachable` in `dataCallback` of `ImageLoad`

--- src/browser/Frame.zig
+++ src/browser/Frame.zig
@@ -2471,7 +2471,6 @@ const ImageLoad = struct {
 
     fn dataCallback(_: *HttpClient.Transfer, _: []const u8) !void {
         // Nothing should ever reach here.
-        unreachable;
     }
 
     fn doneCallback(ctx: *anyopaque) !void {
